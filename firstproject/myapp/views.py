794b54a05b8f3de4eed4257f4b10d245707202c6
Arnav <arnavprajapati3101@gmail.com>
2026-08-17 13:53:19 +0530

    Link myapp URLs with project

--- firstproject/myapp/views.py
+++ firstproject/myapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("Hello, World!")
+
+def about(request):
+    return HttpResponse("welcome to about page")
+
+def contact(request):
+    return HttpResponse("welcome to contact page")
+
+def gallery(request):
+    return HttpResponse("welcome to gallery page")
